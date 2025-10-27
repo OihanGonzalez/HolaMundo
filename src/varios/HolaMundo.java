dd481e8068f7b3ccef2f0afd90e24c3d634afed0
OihanGonzalez <gonzalezoihan8@gmail.com>
2025-10-27 13:55:16 +0100

    Start

---
+++ src/varios/HolaMundo.java
@@ -0,0 +1,16 @@
+package varios;
+
+import java.util.Scanner;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Hola Mundo!");
+		sc.nextLine();
+		
+		sc.close();
+	}
+}
